1cb79d1550b421301dacc5cd6b7a3a8e2d53ce9a
Marina Kent <mtkent@hotmail.com>
2017-02-07 13:02:57 -0800

    edited classes, started crossover

--- classes.py
+++ classes.py
@@ -1,14 +1,21 @@
 import math
+import random
 import subprocess 
 import os
 
-class Shape:
+class Node:
+	def __str__(self):
+		return "unimplemented"
+	def __init__(self, *children):
+		self.children = children
+
+class Shape(Node):
 	def __str__(self):
 		return "{} [{}]".format(self.name, self.argsStr() )
-	def __init__(self, args):
-		self.args = args
+	def __init__(self, *args):
+		super().__init__(*args)
 	def argsStr(self):
-		return " ".join("{} {}".format(k, str(self.args[k])) for k in self.args)
+		return " ".join(str(c)for c in self.children)
 
 class Square(Shape):
 	name = "SQUARE"
@@ -19,15 +26,56 @@ class Circle(Shape):
 class Triangle(Shape):
 	name = "TRIANGLE"
 
+
+class Modifier:
+	def __str__(self):
+		return "{} {}".format(self.name, " ".join(str(v) for v in self.values))
+	def __init__(self, *values):
+		self.values = values
+
+class Alpha(Modifier):
+	name = "a"
+
+class Brightness(Modifier):
+	name = "b"
+
+class Saturation(Modifier):
+	name = "sat"
+
+class Hue(Modifier):
+	name = "h"
+
+class Y(Modifier):
+	name ="y"
+
+class Z(Modifier):
+	name ="z"
+
+class Rotate(Modifier):
+	name = "r"
+
+class Flip(Modifier):
+	name = "f"
+
+class X(Modifier):
+	name = "x"
+
+class Size(Modifier):
+	name = "s"
+
+# takes 1, 2, 4, 6
+class Transform(Modifier):
+	name = "trans"
+
+# takes 2
+class Skew(Modifier):
+	name = "skew"	
+
 class Value:
 	def __str__(self):
 		return repr(self.val)
 	def __init__(self, val):
 		self.val = val
-
-# class Number, variable - name that is a string, want without quotes 
-# repr of a string 
-# repr of a string adds quotes
 
 class Operator:
 	def __str__(self):
@@ -48,15 +96,11 @@ class Multiply(Operator):
 class Divide(Operator):
 	opsStr = "/"
 
-# class parameter also 
-
-
-
-
-
-
-test = Square({"x":Value(2), "b":Value(.5)}) #[Plus(Value(3), Value(5)), Value(4)])
+test = Triangle(Skew(2, 30), Brightness(.5)) 
 print (str(test))
+
+test2 = Square(Transform(2, 30), Flip(.5))
+print (str(test2))
 
 def templateBuild(object):
 	return """
@@ -67,7 +111,6 @@ def templateBuild(object):
 	{}
 	}}
 	""".format(str(object))
-	
 
 def createImage(code):
 	if (not os.path.exists("output")):
@@ -78,5 +121,61 @@ def createImage(code):
 
 	subprocess.run(["ContextFree/ContextFreeCLI.exe", "output/code.cfdg", "output/result.png"])
 
-createImage(templateBuild(test))
+r = random.uniform(0, 50)
 
+name = test
+
+if (r > 25):
+	name = test2 
+
+
+createImage(templateBuild(name))
+
+# def createImage2(code):
+# 	if (not os.path.exists("output")):
+# 		os.mkdir("output")
+
+# 	with open("output/code.cfdg", "w") as fout:
+# 		fout.write(code)
+
+# 	subprocess.run(["ContextFree/ContextFreeCLI.exe", "output/code.cfdg", "output/result2.png"])
+
+# createImage2(templateBuild(test2))
+
+print(str(templateBuild(test)))
+
+print(str(templateBuild(test2)))
+
+
+# will randomly pick left or right first, set left to current
+# def crossover(left, right):
+# 50 percent return copy of left (new node that is equivilant, except with crossover function applied to each child)
+# if type(left) (use is instance?) = node - check if node, not a number 
+# if random less than percent
+# 	crossover: return  crossover (find_crossover(right, left))
+# else
+# 	don't crossover: result type (left)()
+# 		for c in left.children : result.append (Crossover (c, right))
+
+	# preferentially crossover with similar size - need a function that finds the best crossovers
+	# takes in a node, node type, node size 
+	# first check probability of crossover. Need to get a height function in node - do children have children? recurse. 
+
+
+
+# class Number, variable - name that is a string, want without quotes 
+# repr of a string adds quotes
+# class parameter also 
+
+
+
+
+# Made new modifiers, of different types - good idea? - something off about the number of args?
+# Want to be able to use in program - how?
+
+# crossover: if modifiers have same type, can interchange. Shapes can also switch. 
+# can use mutation for numbers. 
+# will want to be able to crossover body of shape 
+
+
+# shape definition will be tricky with the types 
